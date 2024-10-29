b326a6d6af3d9744c56db3ab5b64bd1621db9078
Kirill <chanchinkv@gmail.com>
2024-10-29 19:41:04 +0300

    Was removed excess annotation for active profile choosing.

--- src/test/java/com/javarush/jira/JiraRushApplicationTests.java
+++ src/test/java/com/javarush/jira/JiraRushApplicationTests.java
@@ -1,9 +1,7 @@
 package com.javarush.jira;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 class JiraRushApplicationTests extends BaseTests {
     @Test
     void contextLoads() {
